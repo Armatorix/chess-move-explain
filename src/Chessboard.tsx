6692fd2e2306123016b9d54b2ecb480dfc5da920
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-02-12 22:18:46 +0100

    show more about field

--- src/Chessboard.tsx
+++ src/Chessboard.tsx
@@ -1,13 +1,16 @@
-import { ShortMove } from "chess.js";
+import { ShortMove, Square } from "chess.js";
 import Chessboard from "chessboardjsx";
-import { useRecoilValue } from "recoil";
-import { fenState } from "./store";
+import { useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { fenState, mouseHoverSquareState } from "./store";
 
 export default function ChessboardWithLogic(props: {
   moveHandler: (move: ShortMove) => void;
 }) {
   const fen = useRecoilValue(fenState);
-
+  const [hoverSquare, setHoverSquare] = useRecoilState(mouseHoverSquareState);
+  const squareStyle =
+    hoverSquare === null ? {} : { [hoverSquare]: { backgroundColor: "red" } };
   return (
     <Chessboard
       position={fen}
@@ -18,6 +21,13 @@ export default function ChessboardWithLogic(props: {
           promotion: "q",
         })
       }
+      onMouseOverSquare={(square: Square) => {
+        setHoverSquare(square);
+      }}
+      onMouseOutSquare={() => {
+        setHoverSquare(null);
+      }}
+      squareStyles={squareStyle}
     />
   );
 }
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,12 @@
+import { Square } from "chess.js";
 import { atom } from "recoil";
 
 export const fenState = atom({
   key: "fenState",
   default: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+});
+
+export const mouseHoverSquareState = atom({
+  key: "mouseHoverSquareState",
+  default: null as null | Square,
 });
